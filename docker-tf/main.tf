eb1c8017f5ef0bc54d6fc79dce1b548cc7db7f46
Venu Potanker <p_venu@yahoo.com>
2023-03-13 18:17:08 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,36 @@
+/*
+File name - main.tf
+Author - Venu Potanker
+Version - 0.01
+Description - This is a Terraform learning script
+*/
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+#Listing the provider in below block
+
+provider "docker" {}
+
+//Listing the resources in below block
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
